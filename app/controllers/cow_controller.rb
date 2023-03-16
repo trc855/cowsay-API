ab5b5914ab28606cd0b7959a943fd809ea06db8b
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-03-16 21:10:47 +1100

    added strong params to cow#say

--- app/controllers/cow_controller.rb
+++ app/controllers/cow_controller.rb
@@ -1,8 +1,11 @@
 class CowController < ApplicationController
   def say
+    params.require(:message)
+    params.permit(:cow, :balloon_type, :face_type)
+
     message = params[:message]
     cow = params[:cow] || 'cow'
-    balloon_type = params[:ballon_type] || 'say'
+    balloon_type = params[:balloon_type] || 'say'
     face_type = params[:face_type] || 'default'
 
     @message = Cow.new(cow:, face_type:).say(message, balloon_type)
