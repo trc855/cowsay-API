62e797e8dcdd937acf1d2c2ab96a0594cc215372
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-03-16 21:08:31 +1100

    added support for additional cowsay params

--- app/controllers/cow_controller.rb
+++ app/controllers/cow_controller.rb
@@ -1,5 +1,10 @@
 class CowController < ApplicationController
   def say
-    @message = Cow.new.say(params[:message])
+    message = params[:message]
+    cow = params[:cow] || 'cow'
+    balloon_type = params[:ballon_type] || 'say'
+    face_type = params[:face_type] || 'default'
+
+    @message = Cow.new(cow:, face_type:).say(message, balloon_type)
   end
 end
